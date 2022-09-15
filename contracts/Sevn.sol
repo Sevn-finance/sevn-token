3d0f6ed9439a7068613430bcd9274656a2f53346
Sevn-finance <Sevn.fi@gmail.com>
2022-09-15 21:11:56 +0300

    Update tokenomics
    Changes: update preMineSupply

--- contracts/Sevn.sol
+++ contracts/Sevn.sol
@@ -7,7 +7,7 @@ import "./library/Mintable.sol";
 
 contract Sevn is ERC20, Ownable, Mintable{
 
-    uint256 public constant preMineSupply = 12000000 * 1e18; // 12 000 000
+    uint256 public constant preMineSupply = 45350000 * 1e18; // 45 350 000
     uint256 public constant maxSupply = 350000000 * 1e18; // 350 000 000
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol){
